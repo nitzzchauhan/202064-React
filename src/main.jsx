90c83d0ce87935153d053278636d9f2eb4a86e0b
John Doe <johndoe@example.com>
2024-11-19 12:30:38 +0530

    dgfgf

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,10 @@
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-function Task() {
-  return (
-    <>
-      <h1>To do List</h1>
-      <ul>
-        {taskList.map((t,index,tasklist)=>{
-          return <h1>{t} and total tasks are -- {taskList.join(",")}</h1>
-        })}
-      </ul>
-    </>
-  );
-}
-
-const taskList = ["task1", "task2", "task3"];
-
-createRoot(document.getElementById("root")).render(
-  <>
-    <Task />
-    {Task()}
-  </>
-);
-
-//&&
-
-// {condition && expression}
-  {/* {students.length > 0 && (
-          <h1>There are {students.length} students in the class</h1>
-        ) }  */}
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
